b779e840e4d738036998d07cd2a77ad447c6904d
cmnelso5 <cnelson0641@gmail.com>
2014-03-27 00:04:25 -0400

    Done for the night.  Need to test all UI methods, provide login features, and glue everything together.

--- Health_Interactions/src/frontEnd/healthPro/ProViewPatientInfo.java
+++ Health_Interactions/src/frontEnd/healthPro/ProViewPatientInfo.java
@@ -1,42 +1,42 @@
 package frontEnd.healthPro;
 
+import java.util.List;
 import java.util.Scanner;
+
+import dao.oracle.PatientDAO;
+import frontEnd.utility.Utility;
+import beans.Patient;
+import beans.Physician;
+
+/**
+ * Prints out information on the patient selected by the physician.
+ */
 
 public class ProViewPatientInfo {
 
-	int did;
-	int[] patients_pid;
-	int choice_pid;
-	
-	public ProViewPatientInfo(int did) {
-		this.did = did;
-	}
-	
-	public void drive() {
-		getPatients();
-		getChoice();
-		displayPatientInfo();		
-	}
-	
-	private void getPatients() {
-		// TODO link with cote
-		// patients = ;
-	}
-	
-	private void getChoice() {
-		for (int i=0; i<patients_pid.length; i++) {
-			System.out.println(i + " -- " + getName(patients_pid[i]));
+	/**
+	 * Prints out information on the patient selected by the physician.
+	 */
+	public static void drive() {
+		System.out.println("Enter the patient to view info on: ");
+		List<Patient> list = PatientDAO.viewAllPatients();
+		for (int i=0; i<list.size(); i++) {
+			System.out.println(i + " -- " + list.get(i).getLname() + ", " + list.get(i).getFname());
 		}
-		Scanner scan = new Scanner(System.in);
-		choice_pid = scan.nextInt();
+		int choice = Utility.getValidChoice(list.size());
+		Patient patient = list.get(choice);
+		printPatientInfo(patient);
 	}
 
-	private String getName(int i) {
-		// TODO link with cote
-		return null;
-	}
-	
-	private void displayPatientInfo() {
-		// TODO link with cote
+	/**
+	 * Prints information on the given patient.
+	 * @param patient
+	 */
+	private static void printPatientInfo(Patient patient) {
+		System.out.println(patient.getLname() + ", "+ patient.getFname());
+		System.out.println(patient.getSexAsString());
+		System.out.println(patient.getDobAsString());
+		System.out.println(patient.getAddress());
+		System.out.println(patient.getCity() + "," + patient.getState() + " " + patient.getZip());
 	}
 }
