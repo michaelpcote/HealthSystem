b77afcd0dc55efdb4187ab767fe95bf3f3078d4a
Michael Cote <mpcote@ncsu.edu>
2014-03-27 20:42:58 -0400

    Sample data finished and Fixed health friend issue Chrissy noticed.

--- Health_Interactions/src/dao/oracle/HealthFriendsDAO.java
+++ Health_Interactions/src/dao/oracle/HealthFriendsDAO.java
@@ -27,9 +27,10 @@ public class HealthFriendsDAO {
         	// Get a connection to the specified JDBC URL.
     		conn = JDBCConnection.getConnection();
     		String query = "SELECT p.pid, p.password, p.fname, p.lname, p.address, p.city, p.state, p.zip, ";
-            query += "p.dob, p.sex, p.public_status FROM patients p WHERE p.pid <> ? AND p.public_status = 'yes' AND ";
-            query += "p.pid IN ( SELECT pc.pid FROM patient_conditions pc, health_friends hf WHERE hf.pid = ? AND ";
-            query += "pc.pid <> hf.hf_pid AND pc.cid IN ( SELECT pc2.cid FROM patient_conditions pc2 WHERE pc2.pid = ? ) )";
+            query += "p.dob, p.sex, p.public_status FROM patients p, health_friends hf WHERE p.pid <> ? ";
+            query += "AND p.public_status = 'yes' AND hf.pid = ? AND p.pid <> hf.hf_pid AND ";
+            query += "p.pid IN ( SELECT pc.pid FROM patient_conditions pc WHERE ";
+            query += "pc.cid IN ( SELECT pc2.cid FROM patient_conditions pc2 WHERE pc2.pid = ? ) )";
 			ps = conn.prepareStatement(query);
     		ps.setDouble( 1, patient.getPid());
     		ps.setDouble( 2, patient.getPid());
